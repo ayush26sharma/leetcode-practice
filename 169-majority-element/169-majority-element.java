cd879aa9818dd12e43bcdd666f38b3afdd6ffa1c
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-01-29 20:30:23 +0530

    Time: 20 ms (14.78%), Space: 55.8 MB (8.84%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -13,7 +13,6 @@ class Solution {
         int m = 0;
         int j = 0;
         for (int i = 0; i<nums.length;i++){
-            //System.out.println(hash.get(A[i]));
             if(m<hash.get(nums[i])){
                 m = hash.get(nums[i]);
                 j = nums[i];
