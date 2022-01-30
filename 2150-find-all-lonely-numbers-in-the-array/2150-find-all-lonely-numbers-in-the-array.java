17a043ac03b2299467008b06bd79e425b624c9e0
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-01-31 03:42:39 +0530

    Time: 663 ms (5.02%), Space: 145.6 MB (59.18%) - LeetHub

---
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findLonely(int[] nums) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        List<Integer> arr = new ArrayList<>();
+        for(int i: nums){
+            if(map.containsKey(i)) map.put(i,map.get(i)+1);
+            else map.put(i,1);
+        }
+        
+        for(int i : nums){
+            if (map.get(i)==1){
+                if (!map.containsKey(i+1) && !map.containsKey(i-1)) arr.add(i);
+            }
+        }
+        return arr;
+    }
+}
