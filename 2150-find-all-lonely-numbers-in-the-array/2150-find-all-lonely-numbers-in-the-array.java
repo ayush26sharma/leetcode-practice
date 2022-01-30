c9571149840f6e1e9c02df57f1011a4136c70220
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-01-31 03:48:11 +0530

    Time: 26 ms (99.68%), Space: 61.6 MB (87.07%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -1,15 +1,17 @@
 class Solution {
-    public List<Integer> findLonely(int[] nums) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        List<Integer> arr = new ArrayList<>();
-        for(int i: nums){
-            if(map.containsKey(i)) map.put(i,map.get(i)+1);
-            else map.put(i,1);
+    public List<Integer> findLonely(int[] arr) {
+        ArrayList<Integer> al=new ArrayList<>();
+        Arrays.sort(arr);
+        if(arr.length==1){
+            al.add(arr[0]);
+            return al;
         }
-        
-        for(int i : nums){
-            if (map.get(i)==1 && !map.containsKey(i+1) && !map.containsKey(i-1)) arr.add(i);
+        if(arr[0]+1!=arr[1]&&arr[0]!=arr[1])al.add(arr[0]);
+        if(arr[arr.length-1]-1!=arr[arr.length-2]&&arr[arr.length-1]!=arr[arr.length-2])al.add(arr[arr.length-1]);
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i]==arr[i-1]||arr[i]==arr[i+1])continue;
+            else if(arr[i-1]+1!=arr[i]&&arr[i+1]-1!=arr[i])al.add(arr[i]);
         }
-        return arr;
+        return al;
     }
 }
