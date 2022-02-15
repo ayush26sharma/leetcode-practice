5cc51a66ab24d6e2a5795ecd46eebe2ac189b247
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-02-15 15:32:52 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (23.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int fib(int n) {
         if (n ==0) return 0;
         if (n==1) return 1;
-        if (n==2) return 1;
+        // if (n==2) return 1;
         int [] arr = new int [n];
         arr[0]= 1;
         arr[1] = 1;
