0ff28275443735ceead0d09529851296b6bac02c
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-05-23 02:12:23 +0530

    Time: 1 ms (61.03%), Space: 42.5 MB (56.20%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -14,20 +14,16 @@ class Solution {
             if (mid != 0){
                 if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) return mid;
                 else{
-                if (nums[mid]>nums[nums.length-1]) low = mid + 1;
-                else high = mid - 1;
-            }
-            }
-            else if (mid == 0){
-                if (nums[mid] > nums[mid+1]) return mid;
-                else{
-                if (nums[mid]>nums[nums.length-1]) low = mid + 1;
-                else high = mid - 1;
-            }
+                    if (nums[mid]>nums[nums.length-1]) low = mid + 1;
+                    else high = mid - 1;
+                }
             }
             else{
-                if (nums[mid]>nums[nums.length-1]) low = mid + 1;
-                else high = mid - 1;
+                if (nums[mid] > nums[mid+1]) return mid;
+                else{
+                    if (nums[mid]>nums[nums.length-1]) low = mid + 1;
+                    else high = mid - 1;
+                }
             }
         }
         return -1;
