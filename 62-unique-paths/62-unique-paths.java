b153d9dd6ad271b0b7645eb56707cac6522f0d14
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-06-22 03:22:13 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (54.92%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,7 +4,7 @@ class Solution {
         int j = m-1;         //2
         double res = 1;
         for(int i = 1; i<=j; i++){
-            System.out.println(res);
+            // System.out.println(res);
             res = res*(x-j+i)/i;           // (3-2+1)/1       (3-2+2)/2
         }
         return (int) res;
