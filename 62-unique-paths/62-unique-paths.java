772ad8668e8fee0b932369602f38c9fb0ecaafbb
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-06-22 03:22:04 +0530

    Time: 11 ms (5.03%), Space: 41.7 MB (9.86%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int x = m+n-2;       //3
+        int j = m-1;         //2
+        double res = 1;
+        for(int i = 1; i<=j; i++){
+            System.out.println(res);
+            res = res*(x-j+i)/i;           // (3-2+1)/1       (3-2+2)/2
+        }
+        return (int) res;
+    }
+}
